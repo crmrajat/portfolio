8213dce0d19e75f5a320798f287964cf82bf077a
crmrajat <crmrajat@gmail.com>
2022-09-10 18:12:25 +0530

    'hosting'

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Introduction from './Introduction';
-import Navbar from './Navbar';
-import Projects from './Projects';
-import Skills from './Skills';
-import Experience from './Experience';
-
-export { Introduction, Navbar, Projects, Skills, Experience };
