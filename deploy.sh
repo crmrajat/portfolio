84ea355d4d5da81b94bed0e87f10c68993ac3ca1
crmrajat <crmrajat@gmail.com>
2022-09-10 06:34:28 +0530

    testing

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,24 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
+# git init
+# git checkout -b main
+# git add -A
+# git commit -m 'deploy'
 
-# if you are deploying to https://crmrajat.github.io
-# git push -f git@github.com:crmrajat/crmrajat.github.io.git main
+# # if you are deploying to https://crmrajat.github.io
+# # git push -f git@github.com:crmrajat/crmrajat.github.io.git main
 
-# if you are deploying to https://crmrajat.github.io/portfolio
-git push -f git@github.com:crmrajat/portfolio.git main:gh-pages
+# # if you are deploying to https://crmrajat.github.io/portfolio
+# git push -f git@github.com:crmrajat/portfolio.git main:gh-pages
+
+git add dist -f
+git commit -m "Deploy"
+git subtree push --prefix dist origin gh-pages
 
 cd -
